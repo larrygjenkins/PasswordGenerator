886da1a97244c951b6109f80735cbd5a1b0a3d07
Sooner Writer <larrygjenkins@gmail.com>
2021-01-13 20:36:36 -0800

    added special characters as a properties within the criteria data object

--- script.js
+++ script.js
@@ -13,19 +13,11 @@ var criteria = {
   lc: "abcdefghijklmnopqrstuvwxyz",
   uc: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   num: "0123456789",
-  // sc: " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
-
-  // var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  // var uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  // var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-  // I need to figure out how to get the special characters to work.
-  // var sc = [" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+  sc: "~`!#$%^&*+=-[]\\\';,/{}|\":<>? "
 
 };
 
-console.log(criteria.lc);
+console.log(criteria.sc);
 
 function generatePassword() {
   var pwlength = parseInt(prompt('How many chars?'));
